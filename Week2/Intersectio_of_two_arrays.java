9ab170d9512bade13ad3e89ced11832a32142d7c
milindbishnoi <milind.curio@gmail.com>
2020-02-20 00:51:08 +0530

    InterSection of 2 arrays updated

--- Week2/Intersectio_of_two_arrays.java
+++ Week2/Intersectio_of_two_arrays.java
@@ -1,12 +1,26 @@
 class Solution {
-    public int firstUniqChar(String s) 
+    public int[] intersection(int[] nums1, int[] nums2) 
     {
-     int[] map=new int[256];
-        for(int i=0;i<s.length();i++)
-            map[s.charAt(i)]++;
-        for(int i=0;i<s.length();i++)
-            if(map[s.charAt(i)]==1)
-            return i;
-        return -1;
+     Set<Integer> set=new HashSet<>();
+        for(int i=0;i<nums1.length;i++)
+        {
+            set.add(nums1[i]);
+        }
+        HashSet<Integer> set2=new HashSet<>();
+        for(int i=0;i<nums2.length;i++)
+        {
+            if(set.contains(nums2[i]))
+            {
+                set2.add(nums2[i]);
+            }
+        }
+        int ans[]=new int[set2.size()];
+        int i=0;
+        for (Integer s : set2) 
+        {
+         ans[i]=s;
+            i++;
+        } 
+        return ans;
     }
 }
